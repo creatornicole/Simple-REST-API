60e595b31136820694c381ed1bd146a251d4bd7c
Nicole Gottschall <ngottschall@web.de>
2023-04-13 10:12:15 +0200

    enables connection to show content of index.html

---
+++ server.js
@@ -0,0 +1,12 @@
+var express  = require('express');
+var path     = require('path');
+var app = express();
+
+
+
+app.listen(3000);
+
+app.route('/')
+  .get(function(req, res){
+    res.sendFile(path.join(__dirname, './','index.html'));
+  })
