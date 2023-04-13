d3e71234725dd5cced41fc305de69001ef0eb452
Nicole Gottschall <ngottschall@web.de>
2023-04-13 10:17:02 +0200

    adds endpoints to server.js

--- server.js
+++ server.js
@@ -10,3 +10,23 @@ app.route('/')
   .get(function(req, res){
     res.sendFile(path.join(__dirname, './','index.html'));
   })
+
+app.route('/user').get(function(req, res){
+
+})
+
+app.route('/user').post(function(req,res){
+
+})
+
+app.route('/user/:userId').delete(function(req,res){
+
+})
+
+app.route('/user/:userId').get(function(req,res){
+
+})
+
+app.route('/user/:userId').put(function(req,res){
+    
+})
